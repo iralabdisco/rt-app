ab824a55c76e69229b05a886e21c3f1844075f78
Jacopo Maltagliati <35559767+jack23247@users.noreply.github.com>
2021-02-19 23:21:06 +0100

    Minimal main, added some details to README.md

--- src/main.c
+++ src/main.c
@@ -1,17 +1,16 @@
 #define _GNU_SOURCE
 
-#define WORKERS 1  // ODO, ...
-
 #include <assert.h>
 #include <pthread.h>
 
 #include "odo.h"
 
-/**
- * @enum workerIds
- * @brief An enumeration of the workers' IDs in the worker definitions array
+/** @enum workerIds
+ * @brief An enumeration of the workers' IDs in the worker definitions array.
+ * Note: insert all of the workers _before_ W_WORKERS, as it is used to
+ * determine the workers array size.
  */
-enum { W_ODO };
+enum { W_ODO, W_WORKERS };
 
 /** @struct worker
  *  @brief A structure representing a worker thread
@@ -25,16 +24,16 @@ typedef struct worker {
     void* (*entry)(void*);
 } worker_t;
 
-int main(void) { //int argc, char** argv) {  // Dispatcher
+int main(void) {  // int argc, char** argv) {  // Dispatcher
     // Set-up
-    worker_t workers[WORKERS]; // Worker definitions array
+    worker_t workers[W_WORKERS];  // Worker definitions array
     workers[W_ODO].entry = O_Worker;
     // Dispatch
-    for (int i = 0; i < WORKERS; i++) {
+    for (int i = 0; i < W_WORKERS; i++) {
         pthread_create(&(workers[i].tid), NULL, workers[i].entry, NULL);
     }
     // Wait for worker termination
-    for (int i = 0; i < WORKERS; i++) {
+    for (int i = 0; i < W_WORKERS; i++) {
         pthread_join(workers[i].tid, NULL);
     }
     pthread_exit(0);  // Failsafe
